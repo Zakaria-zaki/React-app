7623ab210931d6b45f5ead0434cfa4a37e33186a
zattaoui <03zaki95@gmail.com>
2023-08-11 01:37:37 +0200

    Use jsx as an constant

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const reactElement = React.createElement('h1', { className: 'my-class' }, 'Hello World');
+// const reactElement = React.createElement('h1', { className: 'my-class' }, 'Hello World');
 
-const tree = React.createElement('div', { className: 'my-class-div' }, reactElement);
+// const tree = React.createElement('div', { className: 'my-class-div' }, reactElement);
+
+const h1 = <h1 className='my-class'> Hello world JSX !</h1>
 
 root.render(
-  tree
+  h1
 );
 
 // If you want to start measuring performance in your app, pass a function
