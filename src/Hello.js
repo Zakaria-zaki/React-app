fdeb10970335cf2d81ed2324fcf55b4ca13959ba
Zakaria-zaki <03zaki95@gmail.com>
2023-08-24 02:31:36 +0200

    Export & import of the components

---
+++ src/Hello.js
@@ -0,0 +1,7 @@
+export default function HelloTwo() {
+    return <h2>Hello From the seconds components</h2>
+}
+
+export function Hello() {
+    return  <h1>Hello World first components</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
-
-function HelloTwo() {
-  return <h2>Hello From the seconds components</h2>
-}
-
-function Hello() {
-  return <>
-  <h1>Hello World first components</h1>
-  <HelloTwo></HelloTwo>
-</>
-}
+import HelloTwo, { Hello } from './Hello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
   <Hello />
+  <HelloTwo></HelloTwo>
+  </>
 );
